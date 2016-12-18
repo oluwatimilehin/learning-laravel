4dcda9d1599cb090213795e5b272506758b68922
Oluwatimilehin <timad97@Yahoo.com>
2016-12-18 16:48:38 +0100

    Add route for about page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title> About page </title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <link rel="stylesheet" href="">
+
+
+</head>
+
+<body>
+
+
+</body>
+</html>
