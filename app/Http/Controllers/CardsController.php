2c910f38372ea7657945d3c6b4393e3d1d649d1d
Oluwatimilehin <timad97@Yahoo.com>
2016-12-20 07:37:49 +0100

    Add namespace to controller and pass in data to view

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+use App\Card;
 use Illuminate\Http\Request;
 
 class CardsController extends Controller
@@ -9,7 +11,8 @@ class CardsController extends Controller
     //
   public function index()
   {
-      return view('cards.index');
+      $cards = Card::all();
+      return view('cards.index',compact('cards'));
   }
   
        
