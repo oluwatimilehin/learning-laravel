d2e07f2e3f2a1291a9b2c7319ee95a3f83ed79af
Oluwatimilehin <timad97@Yahoo.com>
2016-12-20 09:48:19 +0100

    Add route to show method in CardsController

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/cards', 'CardsController@index');
+Route::get('cards/{card}', 'CardsController@show');
