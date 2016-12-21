30d816fec259816e00727637d4bb710a3df4b0c9
Oluwatimilehin <timad97@Yahoo.com>
2016-12-21 19:54:29 +0100

    Create method for storing form data in NotesController

---
+++ app/Http/Controllers/NotesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Note;
+use App\Card;
+
+class NotesController extends Controller
+{
+    //
+    public function store(Request $request, Card $card){ //We are passing in the card object from our route call. Typehinting.
+        $note = new Note();
+        $note->body = $request->body;
+        $card->note()->save($note);
+        return back();
+    }
+}
