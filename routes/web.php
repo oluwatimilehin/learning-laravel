162ce3bbd2370018fd9f55b60ac5cb664b1bff7f
Oluwatimilehin <timad97@Yahoo.com>
2016-12-21 19:28:01 +0100

    Add route for form action

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/cards', 'CardsController@index');
-Route::get('cards/{card}', 'CardsController@show');
+Route::get('/cards/{card}', 'CardsController@show');
+Route::post('/cards/{card}/notes', 'NotesController@store');
