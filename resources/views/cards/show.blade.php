42407bddbc1d868c6712612774bb9c8ac6c87fd0
Oluwatimilehin <timad97@Yahoo.com>
2016-12-21 18:46:49 +0100

    Add bootstrap form

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -10,6 +10,12 @@
                     <li class="list-group-item">  {{ $note->body }} </li>
                 @endforeach
             </ul>
+            <hr>
+            <h3>Add a New Note </h3>
+            <div class="form-group">
+                <textarea name="body" id="" cols="30" rows="10" class="form-control"> </textarea>
+            </div>
+
         </div>
     </div>
 
