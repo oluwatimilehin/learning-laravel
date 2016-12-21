90750b33cd929b3a4979a38523723e77cc889238
Oluwatimilehin <timad97@Yahoo.com>
2016-12-21 18:17:34 +0100

    Add bootstrap to layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,8 +10,9 @@
     <title> @yield('title')</title>
 
     <link rel="stylesheet" href="{{elixir('css/app.css')}}">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
 
 </head>
 <body>
