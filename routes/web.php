a8d5d0c62f76c5e0dce104aebe93658f30fd0412
Oluwatimilehin <timad97@Yahoo.com>
2016-12-22 19:11:19 +0100

    Add route to update a note

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/cards', 'CardsController@index');
 Route::get('/cards/{card}', 'CardsController@show');
 Route::post('/cards/{card}/notes', 'NotesController@store');
 Route::get('/notes/{note}/edit', 'NotesController@edit');
+Route::patch('notes/{note}', 'NotesController@update');
