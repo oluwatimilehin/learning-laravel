2a6a61e057c05eaa969be46323c4fc1e33976faf
Oluwatimilehin <timad97@Yahoo.com>
2016-12-22 19:08:01 +0100

    Add form to edit a note

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -1,4 +1,13 @@
 @extends('layout')
 @section('content')'
-
+<h1>Edit the Note </h1>
+<form method="POST" action=/>
+    {{ csrf_field() }}
+    <div class="form-group">
+        <textarea name="body" id="" cols="30" rows="5" class="form-control"> {{ $note->body }} </textarea>
+    </div>
+    <div class="form-group">
+        <button class="btn btn-primary" type="submit"> Update Note</button>
+    </div>
+</form>
 @stop
