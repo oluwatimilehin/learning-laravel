bd25378ede9197a2295892c4034f0c21a3481159
Oluwatimilehin <timad97@Yahoo.com>
2016-12-22 18:57:13 +0100

    Add route to edit note

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::get('/about', 'PagesController@about');
 Route::get('/cards', 'CardsController@index');
 Route::get('/cards/{card}', 'CardsController@show');
 Route::post('/cards/{card}/notes', 'NotesController@store');
+Route::get('/notes/{note}/edit', 'NotesController@edit');
