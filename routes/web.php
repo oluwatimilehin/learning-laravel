cea9827f678a3e942801bb074c6ee1d078fac779
Oluwatimilehin <timad97@Yahoo.com>
2016-12-29 17:01:13 +0100

    Experiment with group routing

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,12 @@
 | to using a Closure or controller method. Build something great!
 |
 */
+    Route::get('/', 'PagesController@home');
+    Route::get('/about', 'PagesController@about');
+    Route::get('/cards', 'CardsController@index');
+    Route::get('/cards/{card}', 'CardsController@show')->name('cards');
+    Route::post('/cards/{card}/notes', 'NotesController@store');
+    Route::get('/notes/{note}/edit', 'NotesController@edit');
+    Route::patch('notes/{note}', 'NotesController@update');
 
 
-Route::get('/', 'PagesController@home');
-Route::get('/about', 'PagesController@about');
-Route::get('/cards', 'CardsController@index');
-Route::get('/cards/{card}', 'CardsController@show');
-Route::post('/cards/{card}/notes', 'NotesController@store');
-Route::get('/notes/{note}/edit', 'NotesController@edit');
-Route::patch('notes/{note}', 'NotesController@update');
