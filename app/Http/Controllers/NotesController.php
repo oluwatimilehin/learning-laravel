22221251cdc07223d3d59ca3a2f0b1ffa847d2f7
Oluwatimilehin <timad97@Yahoo.com>
2016-12-29 15:10:22 +0100

    Add user id to form field

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -9,9 +9,9 @@ class NotesController extends Controller
 {
     //
     public function store(Request $request, Card $card){ //We are passing in the card object from our route call. Typehinting.
-        $card->addNote(
-            new Note($request->all()) //$request contains the form data
-        );
+        $note = new Note($request->all());
+        $note->user_id = 1;
+        $card->addNote($note); //$request contains the form data
         return back();
     }
     public function edit(Note $note){
